7fc2c04f20d45fb001776a6547c4169440df8d3d
Jonathan Boston <bostonou@gmail.com>
2015-06-01 07:03:24 -0500

    Init commit

---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject cljs.browser-comms "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3269"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
+  :node-dependencies [[source-map-support "0.2.8"]]
+
+  :plugins [[lein-cljsbuild "1.0.5"]]
+
+  :source-paths ["src" "target/classes"]
+
+  :clean-targets ["out" "out-adv"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {
+                :main cljs.browser-comms.core
+                :output-to "out/cljs/browser_comms.js"
+                :output-dir "out"
+                :optimizations :none
+                :cache-analysis true
+                :source-map true}}
+             {:id "release"
+              :source-paths ["src"]
+              :compiler {
+                :main cljs.browser-comms.core
+                :output-to "out-adv/cljs/browser_comms.min.js"
+                :output-dir "out-adv"
+                :optimizations :advanced
+                :pretty-print false}}]})
